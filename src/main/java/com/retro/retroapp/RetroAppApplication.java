ccaa67a63dcf8e8d42f21f3980f9ade1fdc85f57
augustas3000 <augusto.pasaulis@gmail.com>
2021-04-10 17:48:40 +0300

    initial commit

---
+++ src/main/java/com/retro/retroapp/RetroAppApplication.java
@@ -0,0 +1,13 @@
+package com.retro.retroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retro/retroapp/RetroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retro.retroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

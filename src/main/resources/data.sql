e869c276ceb30229cc6e23582f7b35f46b5c6942
augustas3000 <augusto.pasaulis@gmail.com>
2021-04-12 20:13:11 +0300

    feature-add-controllers-with-spring-web-mvc: controller added and tested along with spring security config and a view

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO retro_app_user (id, username, password, role) VALUES
-  (1, 'Zoja', '1236', 'DEV'),
-  (2, 'Kirk', '1254', 'DEV');
-
-INSERT INTO retro_app_comment (id, comment, created_by, created_date, type) VALUES
-  (1, 'That was good', 'Zoja', '2020-02-02', 'PLUS'),
-  (2, 'That was bad', 'Kirk', '2020-02-02', 'DELTA');
